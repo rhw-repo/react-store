25f97fb6815b3aa0dede730276462f77812009cd
Ruth Westnidge <rwestnidge@gmail.com>
2026-09-23 09:09:19 +0000

    Add box-shadow to container for uniformity ascross app. Adjust image position

--- src/components/BlankPagesTemplate.tsx
+++ src/components/BlankPagesTemplate.tsx
@@ -17,7 +17,7 @@ const BlankPagesTemplate: FC<BlankPagesTemplateProps> = ({
       {/* Handles component display within page */}
       <section className="flex justify-center items-center flex-1 w-full bg-[url('/imgs/error-boundary-bg-image.svg')] bg-cover bg-center">
         {/* Handles content container */}
-        <div className="flex flex-col justify-center items-center landscape:flex-row landscape:h-full landscape:sm:h-[60vh] landscape:md:h-[80vh] landscape:lg:h-auto landscape:xl:flex-col max-w-3xl w-full [@media_(min-width:768px)_and_(max-width:1024px)_and_(orientation:portrait)]:max-w-lg p-5 rounded-xl bg-gray-50 text-neutral-900 text-center text-balance text-2xl font-bold portrait:m-8 portrait:sm:md-4 m-4">
+        <div className="flex flex-col justify-center items-center landscape:flex-row landscape:h-full landscape:sm:h-[60vh] landscape:md:h-[80vh] landscape:lg:h-auto landscape:xl:flex-col max-w-3xl w-full [@media_(min-width:768px)_and_(max-width:1024px)_and_(orientation:portrait)]:max-w-lg p-5 rounded-xl bg-gray-50 text-neutral-900 text-center text-balance text-2xl font-bold shadow-xl shadow-stone-900/30 portrait:m-8 portrait:sm:md-4 m-4">
           {/* Column 1 in landscape */}
           <div className="flex-col landscape:flex-1 landscape:justify-center">
             <h1 className="text-5xl landscape:text-5xl md:text-9xl [@media_(min-width:768px)_and_(max-width:1024px)_and_(orientation:portrait)]:text-5xl text-gray-600 m-4">
@@ -33,7 +33,7 @@ const BlankPagesTemplate: FC<BlankPagesTemplateProps> = ({
             <img
               src="/imgs/error-boundary-image.webp"
               alt="A retro style robot toy stares with a fixed expression."
-              className="object-cover"
+              className="w-full h-full object-cover object-top rounded-sm shadow-media"
             />
           </div>
 
